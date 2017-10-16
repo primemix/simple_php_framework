caa976b93fc4ad9264b548eaf81e78ba6b6a850f
Serhii Kosyouk <monstrpro@gmail.com>
2017-10-16 20:36:55 +0300

    Add htmlspecialchars ENT_QUOTES

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
-$input = $_GET['name'];
+$input = isset($_GET['name']) ? $_GET['name'] : 'World!';
 
-printf('Hello %s', $input);
+header('Contend-Type: text/html; charset=utf-8');
+
+printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
+
