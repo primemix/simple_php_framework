2922d9b8d2ac89d5c313598bb2787febcad2ec95
Serhii Kosyouk <monstrpro@gmail.com>
2017-10-19 21:42:27 +0300

    Add & use unit tests

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -1,42 +1,48 @@
 <?php
 
 namespace Simplex;
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\Tests\Controller;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
-
+use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+/**
+ * Class Framework
+ * @package Simplex
+ */
 class Framework
 {
-protected $matcher;
-protected $controllerResolver;
-protected $argumentResolver;
+    protected $matcher;
+    protected $controllerResolver;
+    protected $argumentResolver;
+    public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $controllerResolver, ArgumentResolverInterface $argumentResolver)
+    {
+        $this->matcher = $matcher;
+        $this->controllerResolver = $controllerResolver;
+        $this->argumentResolver = $argumentResolver;
+    }
+    /**
+     * @param Request $request
+     * @return mixed|Response
+     */
+    public function handle(Request $request)
+    {
+        $this->matcher->getContext()->fromRequest($request);
+        try {
+            $request->attributes->add($this->matcher->match($request->getPathInfo()));
+            $controller = $this->controllerResolver->getController($request);
+            $arguments = $this->argumentResolver->getArguments($request, $controller);
 
-public function __construct(UrlMatcher $matcher, ControllerResolver $controllerResolver, ArgumentResolver $argumentResolver)
-{
-$this->matcher = $matcher;
-$this->controllerResolver = $controllerResolver;
-$this->argumentResolver = $argumentResolver;
-}
-
-public function handle(Request $request)
-{
-$this->matcher->getContext()->fromRequest($request);
-
-try {
-$request->attributes->add($this->matcher->match($request->getPathInfo()));
-
-$controller = $this->controllerResolver->getController($request);
-$arguments = $this->argumentResolver->getArguments($request, $controller);
-
-return call_user_func_array($controller, $arguments);
-} catch (ResourceNotFoundException $e) {
-return new Response('Not Found', 404);
-} catch (\Exception $e) {
-return new Response('An error occurred', 500);
-}
-}
+            return call_user_func_array($controller, $arguments);
+        } catch (ResourceNotFoundException $e) {
+            return new Response('Not Found', 404);
+        } catch (\Exception $e) {
+            return new Response('An error occurred', 500);
+        }
+    }
 }
--- src/pages/hello.php
+++
@@ -1,4 +0,0 @@
-
-<!-- example.com/src/pages/hello.php -->
-
-Hello <?php echo htmlspecialchars(isset($name) ? $name : 'World', ENT_QUOTES, 'UTF-8') ?>
